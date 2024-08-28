23dbc0ff4019f0de25cc2cb96ff869e6bf982f13
LShen123 <77588496+LShen123@users.noreply.github.com>
2024-08-28 16:59:26 -0400

    Addition of ScatterPetal and Radar for Iris

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 import pandas as pd
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
 import os
 from io import BytesIO
 import base64
@@ -12,11 +15,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from model import train_and_predict_linear, train_and_predict_logistic, plot_results
 from sklearn.metrics import accuracy_score
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/about')
@@ -28,11 +32,42 @@ def gallery():
     return render_template('gallery.html')
 
 @app.route('/regression')
-def index():
+def regression():
     # Load the iris dataset
     iris = load_iris()
     iris_pd = pd.DataFrame(iris.data, columns=iris.feature_names)
     iris_pd['target'] = iris.target
+    data = {
+        'features' : iris_pd.columns[:-1].tolist(),
+        'species' : iris_pd['target'].tolist(),
+        'data' : iris_pd.iloc[:,:-1].values.tolist(),
+        'species_names': iris.target_names.tolist()
+    }
+
+    mean_values = iris_pd.groupby('target').mean()
+    radar_data = {
+        'labels': iris.feature_names,  # Feature names
+        'datasets': [
+            {
+                'label': 'Setosa',
+                'data':  mean_values.loc[0].tolist(),
+                'borderColor': 'rgba(255, 99, 132, 1)',
+                'backgroundColor': 'rgba(255, 99, 132, 0.2)'
+            },
+            {
+                'label': 'Versicolor',
+                'data': mean_values.loc[1].tolist(),
+                'borderColor': 'rgba(54, 162, 235, 1)',
+                'backgroundColor': 'rgba(54, 162, 235, 0.2)'
+            },
+            {
+                'label': 'Virginica',
+                'data': mean_values.loc[2].tolist(),
+                'borderColor': 'rgba(75, 192, 192, 1)',
+                'backgroundColor': 'rgba(75, 192, 192, 0.2)'
+            }
+        ]
+    }
 
     # Prepare data for training and prediction using Linear Regression
     _, _, _, y_test_lin, pred_lin = train_and_predict_linear(iris_pd)
@@ -45,7 +80,7 @@ def index():
     # Calculate accuracy for Logistic Regression
     accuracy_log = accuracy_score(y_test_log, pred_log)
 
-    return render_template('regression.html', plot_url_lin=plot_url_lin, plot_url_log=plot_url_log, accuracy_log=accuracy_log)
+    return render_template('regression.html', data=data, radar_data=radar_data, plot_url_lin=plot_url_lin, plot_url_log=plot_url_log, accuracy_log=accuracy_log)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
